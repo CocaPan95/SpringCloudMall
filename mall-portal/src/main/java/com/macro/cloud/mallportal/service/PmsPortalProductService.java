f4f081a555f1afdb3703653c4a471b9fc4800961
2530470001 <2530470001@qq.com>
2023-03-08 13:13:48 +0800

    移动端商品

--- mall-portal/src/main/java/com/macro/cloud/mallportal/service/PmsPortalProductService.java
+++ mall-portal/src/main/java/com/macro/cloud/mallportal/service/PmsPortalProductService.java
@@ -1,12 +1,24 @@
 package com.macro.cloud.mallportal.service;
 
+import com.macro.cloud.mallportal.domain.PmsPortalProductDetail;
 import com.macro.cloud.mallportal.domain.PmsProductCategoryNode;
+import com.macro.cloud.model.PmsProduct;
 
 import java.util.List;
 
 public interface PmsPortalProductService {
     /**
+     * 综合搜索商品
+     */
+    List<PmsProduct> search(String keyword, Long brandId, Long productCategoryId, Integer pageNum, Integer pageSize, Integer sort);
+
+    /**
      * 以树形结构获取所有商品分类
      */
     List<PmsProductCategoryNode> categoryTreeList();
+
+    /**
+     * 获取前台商品详情
+     */
+    PmsPortalProductDetail detail(Long id);
 }
